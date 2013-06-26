a401f71fd93d252ef74351ef558878ee2f350ce9
joseph <joseph@JHP>
2013-06-26 01:11:36 -0700

    closes #7:  Scope should be reset before any test or dependent test methods, I.E. before, are called. bug.

--- src/js/TDDforJSEvaluator.js
+++ src/js/TDDforJSEvaluator.js
@@ -2,6 +2,24 @@
  * @constructor
  */
 function TDDforJSEvaluator(){
+   var __$$__originalGlobalScope={};
+   var __$$__globalScope=(function(){return this;})();
+   var __$$__reset=function(){
+      var __$$__name;
+      for(__$$__name in __$$__globalScope){
+         if(!(__$$__name in __$$__originalGlobalScope)){
+            __$$__globalScope[__$$__name]=void 0;
+         } else {
+            __$$__globalScope[__$$__name]=__$$__originalGlobalScope[__$$__name];
+         }
+      }
+   };
+   var __$$__store=function(){
+      var __$$__name;
+      for(__$$__name in __$$__globalScope){
+         __$$__originalGlobalScope[__$$__name]=__$$__globalScope[__$$__name];
+      }
+   };
    /**
     * @param {string} code
     * @param {Object} mappedResults
--- test/units/TDDforJSEvaluator.js
+++ test/units/TDDforJSEvaluator.js
@@ -13,3 +13,10 @@ function test_mappedResults_should_not_be_accessible(){
       throw "mappedResults was accessible.";
    }
 }
+function test_globally_defined_variables_should_be_accessible(){
+   evaluator.eval("asdf=5;");
+   assert.equal(asdf, 5);
+}
+function test_previously_defined_global_variables_should_not_be_accessible(){
+   assert(typeof asdf === 'undefined');
+}
