95f5926aa2003d6fae5b896d0a39e0ada3886e8d
joseph <joseph@JHP>
2013-07-06 02:40:10 -0700

    beefing up TestSuite to include: classname, package, time, timestamp, failures, errors.  Preserving the ordering of test cases, and allowing for duplicate cases.

--- src/js/TestSuite.js
+++ src/js/TestSuite.js
@@ -3,9 +3,14 @@
  * any before or after methods that need to be run.
  *
  * @constructor
+ * @param {string} prefix Prefix of the testSuiteResults variable used in the
+ * evaled code.  This allows us to unit test this class.
+ * @param {string} className E.G. foo.Foo
  * @param {string} source
  */
 function TestSuite(
+   prefix,
+   className,
    source
 ){
    /** @type {RegExp} */
@@ -16,6 +21,15 @@ function TestSuite(
    var reg_testBefore=/^function\s+before[^a-zA-Z0-9_$]+/m;
    /** @type {RegExp} */
    var reg_blockComments=/\/\*(?:(?!\*\/)[\s\S])*\*\//g;
+   /** @type {RegExp} */
+   var reg_preprocessedComment=/PREPROCESSED_COMMENT[0-9]+/g;
+   /** @type {RegExp} */
+   var reg_className=/^(?:(?:[a-z][a-z0-9]*)\.)*[a-z][a-z0-9]*$/i;
+   /** @type {RegExp} */
+   var reg_simpleClassName=/^(?:.+\.)?([a-z][a-z0-9]*)$/i;
+   /** @type {RegExp} */
+   var reg_package=/^(?:(.+)\.)?[a-z][a-z0-9]*$/i;
+
 
    //INTERNAL STATE
    /** @type {boolean} */
@@ -25,10 +39,26 @@ function TestSuite(
    var hasAfter=false;
    /** @type {boolean} */
    var hasBefore=false;
+   /** @type {string} */
    var testCases=[];
+   /** @type {string} */
+   var testSuiteString="";
+   /** @type {string} */
+   var simpleClassName="";
+   /** @type {string} */
+   var package="";
 
-   if(typeof source !== 'string'){
-      throw "source must be a string representing the contents of a suite.";
+   if(typeof prefix !== 'string'){
+      throw new Error("prefix must be a string to prepend to the testSuiteResults var.");
+   }
+   if(typeof className !== 'string'){
+      throw new Error("className must be a string representing the full classname of the suite.");
+   }
+   if(!source || typeof source !== 'string'){
+      throw new Error("source must be a non empty string representing the contents of a suite.");
+   }
+   if(!reg_className.test(className)){
+      throw new Error("A valid className must be given: "+className);
    }
 
    /**
@@ -68,40 +98,125 @@ function TestSuite(
    };
 
    /**
-    * Returns the test cases.  Each test case has a name and source to execute
-    * against the source of the test suite.
+    * Returns the runnable source of the test suite including test cases.
     *
-    * @returns {Array.<Object>}
+    * @returns {string}
     */
-   this.getTestCases=function(){
+   this.toString=function(){
+      var i;
+      var numberOfTestCases;
+      var testCaseName;
+      var testCase;
+      var results=prefix+"testSuiteResults";
       analyzeTestSuite();
-      return [].concat(testCases);
+      if(!testSuiteString){
+         numberOfTestCases=testCases.length;
+         testSuiteString=[
+            source,
+            "!function(){",
+            results+".testCases=[];",
+            results+".name='"+simpleClassName+"';",
+            results+".package='"+package+"';",
+            results+".tests="+numberOfTestCases+";",
+            results+".errors=0;",
+            results+".failures=0;",
+            //this is just for starters.  Replace it after running the cases.
+            results+".time=Date.now();",
+            results+".timestamp=new Date().toISOString();"
+         ].join('\n');
+         for(i=0;i<numberOfTestCases;i++){
+            testCaseName=testCases[i];
+            testCase=results+".___currentTestCase";
+            testSuiteString+=[
+               '',
+               testCase+"={name:'"+testCaseName+"',className:'"+className+"'};",
+               results+".testCases.push("+testCase+");",
+               prefix+"store();",
+               "!function(){",
+                  testCase+".time=Date.now();",
+                  "try{",
+                     hasBefore?"before();":"",
+                     testCaseName+"();",
+                     hasAfter?"after();":"",
+                  "}catch(e){",
+                     "if(e instanceof Error){",
+                        "if(e.constructor.name.toLowerCase().indexOf('assert') > -1){",
+                           results+".failures++;",
+                           testCase+".failure={",
+                              "message:e.message,",
+                              "type:e.constructor.name,",
+                              "stack:e.stack",
+                           "};",
+                        "} else {",
+                           results+".errors++;",
+                           testCase+".error={",
+                              "message:e.message,",
+                              "type:e.constructor.name,",
+                              "stack:e.stack",
+                           "};",
+                        "}",
+                     "} else {",
+                        results+".failures++;",
+                        testCase+".failure={",
+                           "message:e,",
+                           "type:'unknown'",
+                        "};",
+                     "}",
+                  "}",
+                  testCase+".time=(Date.now()-"+testCase+".time)/1000;",
+               "}();",
+               prefix+"reset();",
+               ''
+            ].join('\n');
+         }
+         testSuiteString+=[
+            '',
+               results+".time=(Date.now()-"+results+".time)/1000;",
+            "}();",
+            ''
+         ].join('\n');
+      }
+      return testSuiteString;
    };
 
    /**
     * Sets up the internal state of this object appropriately.
     */
    function analyzeTestSuite(){
-      var sourceWithoutComments;
-      var testMatch;
-      var testCaseName;
+      var duplicateTestCases={};
+      var comments = {};
+      var commentCounter=0;
+      var duplicateCounter=0;
       if(!isAnalyzed){
-         if(source && typeof source === 'string'){
-            sourceWithoutComments=source.replace(reg_blockComments, "");
-            hasAfter=reg_testAfter.test(sourceWithoutComments);
-            hasBefore=reg_testBefore.test(sourceWithoutComments);
-            while(testMatch=reg_test.exec(sourceWithoutComments)){
-               testCaseName=testMatch[1];
-               testCases.push({
-                  name:testCaseName,
-                  source:[
-                     hasBefore?"before();":"",
-                     testCaseName+"();",
-                     hasAfter?"after();":""
-                  ].join('\n')
-               });
+         package=className.replace(reg_package, "$1");
+         simpleClassName=className.replace(reg_simpleClassName, "$1");
+         source=source.replace(reg_blockComments, function(match){
+            var id = "PREPROCESSED_COMMENT"+commentCounter;
+            comments[id]=match;
+            commentCounter++;
+            return id;
+         });
+         hasAfter=reg_testAfter.test(source);
+         hasBefore=reg_testBefore.test(source);
+         source=source.replace(reg_test, function(match, name){
+            var duplicate;
+            if(name in duplicateTestCases){
+               duplicate="duplicate_"+duplicateCounter+"_"+name;
+               duplicateCounter++;
+               match=match.replace(name, duplicate);
+               testCases.push(duplicate);
+            } else {
+               testCases.push(name);
             }
-         }
+            duplicateTestCases[name]=name;
+            return match;
+         });
+         source=source.replace(
+            reg_preprocessedComment,
+            function(match){
+               return comments[match];
+            }
+         );
          isAnalyzed=true;
       }
    };
--- test/units/TestSuite.js
+++ test/units/TestSuite.js
@@ -1,45 +1,94 @@
 var assert = require('assert');
 var testSuite;
 var results;
+var prefix="_";
+var className="foo.Foo";
+//these are here to support the evaled code from the toString() method.
+var _store;
+var _reset;
+var _testSuiteResults;
+var testFn;
 var sampleSuite=[
    '/*',
    '//Test',
    'function it_should_not_be_considered_when_commented_by_a_block_comment(){}',
    '*/',
    '//Test',
-   'function it_should_be_cool(){test(5);}'
+   'function it_should_be_cool(){testFn(5);}',
+   '//Test',
+   'function it_should_be_cool(){testFn(5);throw 5;}',
+   '//Test',
+   'function $it_should_be_cool(){testFn(5);asdfasdf}',
+   '//Test',
+   'function $it_should_be_cool(){assert(false);}'
 ].join('\n');
 
-function test_constructor_should_force_string(){
+function before(){
+   _store=function(){};
+   _reset=function(){};
+   _testSuiteResults={};
+   testFn=function(){};
+}
+function test_constructor_should_force_prefix_as_string(){
    assert['throws'](function(){
-      new TestSuite(null);
+      new TestSuite(null, className, "asdf");
    });
    assert.doesNotThrow(function(){
-      new TestSuite("asdf");
+      new TestSuite(prefix, className, "asdf");
+   });
+}
+function test_constructor_should_force_className_as_string(){
+   assert['throws'](function(){
+      new TestSuite(prefix, null, "asdf");
+   });
+   assert.doesNotThrow(function(){
+      new TestSuite(prefix, "asdf", "asdf");
+   });
+}
+function test_constructor_should_force_valid_className(){
+   assert['throws'](function(){
+      new TestSuite(prefix, "-------", "asdf");
+   });
+   assert['throws'](function(){
+      new TestSuite(prefix, "asdf.", "asdf");
+   });
+   assert['throws'](function(){
+      new TestSuite(prefix, ".asdf", "asdf");
+   });
+   assert['throws'](function(){
+      new TestSuite(prefix, "", "asdf");
+   });
+}
+function test_constructor_should_force_source_as_string(){
+   assert['throws'](function(){
+      new TestSuite(prefix, className, null);
+   });
+   assert.doesNotThrow(function(){
+      new TestSuite(prefix, className, "asdf");
    });
 }
 function test_hasAfter_should_be_set_appropriately(){
    assert(
-      !new TestSuite(sampleSuite).hasAfter(),
+      !new TestSuite(prefix, className, sampleSuite).hasAfter(),
       "hasAfter should be false by default"
    );
    assert(
-      new TestSuite(sampleSuite+"\nfunction after(){}").hasAfter(),
+      new TestSuite(prefix, className, sampleSuite+"\nfunction after(){}").hasAfter(),
       "hasAfter should be true when after is defined in suite."
    );
 }
 function test_hasBefore_should_be_set_appropriately(){
    assert(
-      !new TestSuite(sampleSuite).hasBefore(),
+      !new TestSuite(prefix, className, sampleSuite).hasBefore(),
       "hasBefore should be false by default"
    );
    assert(
-      new TestSuite(sampleSuite+"\nfunction before(){}").hasBefore,
+      new TestSuite(prefix, className, sampleSuite+"\nfunction before(){}").hasBefore,
       "hasBefore should be true when before is defined in suite."
    );
 }
 function test_no_tests_should_be_defined_by_default(){
-   testSuite = new TestSuite("asdf");
+   testSuite = new TestSuite(prefix, className, "asdf");
    assert(
       !testSuite.hasTestCases() &&
       testSuite.numberOfTestCases() === 0,
@@ -47,18 +96,35 @@ function test_no_tests_should_be_defined_by_default(){
    );
 }
 function test_tests_should_be_added_when_defined_in_a_suite(){
-   testSuite = new TestSuite(sampleSuite);
-   assert(
-      testSuite.hasTestCases() &&
-      testSuite.numberOfTestCases() === 1
-   );
+   testSuite = new TestSuite(prefix, className, sampleSuite);
+   assert(testSuite.hasTestCases(),"there were no test cases defined.");
+   assert.equal(testSuite.numberOfTestCases(), 4, "there were "+testSuite.numberOfTestCases()+" cases. Expected 4.");
 }
 function test_resulting_source_should_execute_test(){
-   var expected;
-   var test = function(a){
-      expected=a+2;
+   var expected=0;
+   var testCases;
+   var testFn = function(a){
+      expected+=1;
    };
-   testSuite=new TestSuite(sampleSuite);
-   eval(sampleSuite+"\n"+testSuite.getTestCases()[0].source);
-   assert.equal(expected, 7);
+   testSuite=new TestSuite(prefix, className, sampleSuite);
+   eval(testSuite.toString());
+   testCases=_testSuiteResults.testCases;
+   [
+      testCases[1],
+      testCases[3]
+   ].forEach(function(v){
+      assert(v.failure, "Errors with assert in Error.prototype.constructor.name.toLowerCase(), or that aren't an instanceof Error should be considered a failure.");
+   });
+   assert(testCases[2].error, "Errors that are an instanceof Error should be considered an error.");
+   assert.equal(testCases[1].name, "duplicate_0_it_should_be_cool", "duplicate tests are prefixed with duplicate.");
+   assert.equal(testCases[3].name, "duplicate_1_$it_should_be_cool", "duplicate tests with $ are prefixed with duplicate.");
+   assert.equal(expected, 3, "tests aren't calling the testFn.");
+   assert.equal(_testSuiteResults.package, "foo", "package set when package exists.");
+   assert.equal(_testSuiteResults.name, "Foo", "name set when package and Class exists.");
+   assert.equal(testCases[0].name, "it_should_be_cool", "name set when package and Class exists.");
+   assert.equal(testCases[0].className, "foo.Foo", "name set when package and Class exists.");
+
+   testSuite=new TestSuite(prefix, "Foo", sampleSuite);
+   eval(testSuite.toString());
+   assert.equal(_testSuiteResults.package, "", "package not set when package does not exist.");
 }
